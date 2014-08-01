575c75fd3273f9bce2c29223a4b4e33ebc1663ad
chamocc <chamocc@gmail.com>
2014-08-01 08:38:57 -0600

    cambios calculadora

--- HDT4/src/Calculadora.java
+++ HDT4/src/Calculadora.java
@@ -1,17 +1,109 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package hdt4;
-
-/**
- *
- * @Daniel Gerendas 13158
- * @Edgar Chamo 13083
- * @since 1/08/2014
- */
-public class Calculadora {
+ *Ejemplo tomado de página:
+*http://chuwiki.chuidiang.org/index.php?title=Lectura_y_Escritura_de_Ficheros_en_Java
+*/
+import java.io.*;
+import java.util.ArrayList;
+ 
+class Calculadora {
+    private Stack<String> operaciones;
+    private ArrayList<Integer> resultados;
+    private Stack<Integer> datos;
     
+   public void Calculadora () {
+      File archivo = null;
+      FileReader fr = null;
+      BufferedReader br = null;
+      operaciones= new StackVector<String>();
+      datos= new StackVector<Integer>();
+      resultados=new ArrayList<Integer>();
+ 
+      //Comienza código tomado de internet
+      try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+          
+         //poner la direccion de la carpeta del archivo en la computadora respectiva
+         archivo = new File ("C:\\Users\\Ed. Chamo\\Desktop\\Trabajos GitHub\\HDT2-Estructura-de-datos\\datos.txt");
+         fr = new FileReader (archivo);
+         br = new BufferedReader(fr);
+ 
+         // Lectura del fichero
+         String linea;
+         int ind=0;
+         while((linea=br.readLine())!=null){
+            operaciones.push(linea);
+         }
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+      }
+      //termina codigo tomado dd internet
+      
+      while(!operaciones.empty()){
+          operarLinea();
+      }
+      System.out.println("=====================================");
+      imprimir();
+      System.out.println("=====================================");
+   }
+   
+   private void operarLinea(){
+       String dato=operaciones.pop();
+       System.out.println(dato);
+       char[] chars=dato.toCharArray();
+       for(int i=0; i<chars.length; i++){
+           if(chars[i]==' '){
+               
+           }else{
+               if(chars[i]== '+' || chars[i]== '-' || chars[i]== '*' || chars[i]== '/'){
+               int op2=datos.pop();
+               //System.out.println(op2);
+               int op1=datos.pop();
+               //System.out.println(op1);
+               datos.push(verificarOperacion(op1, op2, chars[i]));
+               }else{
+                   int datascii=(int)chars[i];
+                   int dat =Character.getNumericValue(datascii);
+                   datos.push(dat);
+               }
+           }
+       }
+       resultados.add(datos.pop());
+       
+   }
+   
+   private int verificarOperacion(int op1, int op2, char car){
+       int resultado=0;
+       if(car=='+'){
+           resultado=op1+op2;
+       }
+       if(car=='-'){
+           resultado=op1-op2;
+       }
+       if(car=='*'){
+           resultado=op1*op2;
+       }
+       if(car=='/'){
+           resultado=(int)op1/op2;
+       }
+       return resultado;
+   }
+   
+   private void imprimir(){
+       for(int i=0; i<resultados.size(); i++){
+           System.out.println("Resultado:\n"+resultados.get(i));
+       }
+   }
 }
