a3656691bb0b4e993fe2621918616654038c3d40
chamocc <chamocc@gmail.com>
2014-08-01 08:11:54 -0600

    ejemplos de las listas

--- HDT4/src/StackArrayList.java
+++ HDT4/src/StackArrayList.java
@@ -15,33 +15,38 @@ public class StackArrayList<E>
 		data = new ArrayList<E>();
 	}
 
+        @Override
 	public void push(E item)
 	// post: the value is added to the stack
 	//          will be popped next if no intervening push
 	{
 		data.add(item);
 	}
-
+        
+        @Override
 	public E pop()
 	// pre: stack is not empty
 	// post: most recently pushed item is removed and returned
 	{
 		return data.remove(size()-1);
 	}
-
+        
+        @Override
 	public E peek()
 	// pre: stack is not empty
 	// post: top value (next to be popped) is returned
 	{
 		return data.get(size() - 1);
 	}
-	
+        
+	@Override
 	public int size()
 	// post: returns the number of elements in the stack
 	{
 		return data.size();
 	}
   
+        @Override
 	public boolean empty()
 	// post: returns true if and only if the stack is empty
 	{
