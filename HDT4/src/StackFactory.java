002c7fa977d7ce7be93202c1a4f0008ad50ae411
chamocc <chamocc@gmail.com>
2014-08-04 13:12:30 -0600

    StackFactory

--- HDT4/src/StackFactory.java
+++ HDT4/src/StackFactory.java
@@ -1,8 +1,25 @@
+
+import java.util.Scanner;
+
 /**
  * @author Daniel Gerendas 13158
  * @author Edgar Chamo 13083
  * @since 1/08/2014
  */
 public class StackFactory {
-    
+    public Stack getStack(){
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Ingrese Implementacion del Stack: \n0: StackVector\n1: StackList\n2: StackArrayList\n");
+        int param=entrada.nextInt();
+        entrada.nextLine();
+        switch (param){
+            case 0:
+                return new StackVector();
+            case 1:
+                return new StackList();
+            case 2:
+                return new StackArrayList();
+        }
+        return null;
+    }
 }
