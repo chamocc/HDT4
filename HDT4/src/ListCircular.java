ebf801b1d0137a2d138f28c8ab9e637fbfe8f168
chamocc <chamocc@gmail.com>
2014-08-04 16:40:43 -0600

    encabezados

--- HDT4/src/ListCircular.java
+++ HDT4/src/ListCircular.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author Daniel
+ * @author Daniel Gerendas 13158
+ * @author Edgar Chamo 13083
+ * @since 1/08/2014
  */
 public class ListCircular<E> extends AbstractList<E> {
     
